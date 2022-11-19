17c9fab42fa8ae7afed19312e218d899a2985237
AlonsoYuen19 <wwepg15@gmail.com>
2022-11-19 01:14:57 -0500

    update
     chatbot

--- lib/pages/patient/chatbot.dart
+++ lib/pages/patient/chatbot.dart
@@ -1,3 +1,4 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:flutter/material.dart';
 
 class ChatBot extends StatefulWidget {
@@ -8,15 +9,143 @@ class ChatBot extends StatefulWidget {
 }
 
 class _nameState extends State<ChatBot> {
+  final TextEditingController _controller = TextEditingController();
+  late DialogFlowtter dialogFlowtter;
+  List<Map<String, dynamic>> messages = [];
+  @override
+  void initState() {
+    super.initState();
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+  }
+
+  void sendMessage(String text) async {
+    if (text.isEmpty) {
+      return;
+    }
+    setState(() {
+      Message userMessage = Message(text: DialogText(text: [text]));
+      addMessage(userMessage, true);
+    });
+    QueryInput query = QueryInput(text: TextInput(text: text));
+    DetectIntentResponse res = await dialogFlowtter.detectIntent(
+      queryInput: query,
+    );
+    if (res.message == null) return;
+    setState(() {
+      addMessage(res.message!);
+    });
+  }
+
+  void addMessage(Message message, [bool isUserMessage = false]) {
+    messages.add({
+      'message': message,
+      'isUserMessage': isUserMessage,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Asistente Virtual"),
+      appBar: AppBar(title: Text('Asistente Virtual')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+                child: MessageList(
+              messages: messages,
+            )),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              color: Colors.blue,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      cursorColor: Colors.white,
+                      controller: _controller,
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.send),
+                    onPressed: () {
+                      sendMessage(_controller.text);
+                      _controller.clear();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Container(
-        child: Text("ChatBot"),
-      ),
+    );
+  }
+}
+
+class MessageList extends StatelessWidget {
+  const MessageList({Key? key, this.messages = const []}) : super(key: key);
+  final List<Map<String, dynamic>> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: messages.length,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      separatorBuilder: (_, i) => const SizedBox(height: 10),
+      itemBuilder: (context, i) {
+        var obj = messages[messages.length - 1 - i];
+        return _MessageContainer(
+          message: obj['message'],
+          isUserMessage: obj['isUserMessage'],
+        );
+      },
+      reverse: true,
+    );
+  }
+}
+
+class _MessageContainer extends StatelessWidget {
+  final Message message;
+  final bool isUserMessage;
+
+  const _MessageContainer({
+    Key? key,
+    required this.message,
+    this.isUserMessage = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          isUserMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          constraints: BoxConstraints(maxWidth: 250),
+          child: Container(
+            decoration: BoxDecoration(
+              color: isUserMessage ? Colors.blue : Colors.green,
+              borderRadius: BorderRadius.circular(20),
+            ),
+
+            /// Espaciado
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              message.text?.text![0] ?? '',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
